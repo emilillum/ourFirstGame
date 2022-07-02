31f897a2ecd2a0baa3f5a2b82c2f3e33b65d2b1b
emilillum <emil.vdk@gmail.com>
2022-07-02 16:33:21 +0200

    changed the bird to have it's own file

--- flappybird.py
+++ flappybird.py
@@ -1,8 +1,10 @@
 import arcade
 
+image_source = "yellowbird-midflap.png"
+center_x = 50
+center_y = 350
+
 class Bird(arcade.Sprite):
 
-    def __init__(self, image_source, center_x, center_y):
+    def __init__(self):
         super().__init__(filename=image_source, center_x=center_x, center_y=center_y)
-    
-        
